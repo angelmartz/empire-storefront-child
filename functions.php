0764bdaec95f6e1f20f86eb52268ee52de9f1c9f
Jean Kim <jeankim29@gmail.com>
2016-01-30 20:42:02 -0500

    cleanup

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 
 // Establish relationship between parent and child themes
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
