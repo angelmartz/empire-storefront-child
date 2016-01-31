e47c0d233d77ce27aabee950ee558e2b3b323217
Jean Kim <jeankim29@gmail.com>
2016-01-30 23:40:09 -0500

    refactored homepage carousel markup

--- functions.php
+++ functions.php
@@ -128,7 +128,22 @@ function empire_homepage_featured() {
     setup_postdata( $post );
     $background_image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 
-    echo '<div class="carousel-item" style="background-image: url(' . $background_image_url . ')"><div class="carousel-item-content"><div class="border"><div class="background"><a href="' . get_permalink($post->ID) . '"><h1>' . $post->post_title . '</h1><p>' . $post->post_excerpt . '</p></a></div></div></div></div>';
+    ?>
+
+    <div class="carousel-item" style="background-image: url('<?php echo $background_image_url ?>')">
+      <div class="carousel-item-content">
+        <div class="border">
+          <div class="background">
+            <a href="<?php echo get_permalink($post->ID) ?>">
+              <h1><?php echo $post->post_title ?></h1>
+              <p><?php echo $post->post_excerpt ?></p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>';
+
+<?php
 
   }
 
