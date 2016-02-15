7977ae59dc8863c35ab03af6a989474ba254aa59
Jean Kim <jeankim29@gmail.com>
2016-02-14 23:18:17 -0500

    add blurb on top of member discount page + rename div

--- archive-etc_member_discounts.php
+++ archive-etc_member_discounts.php
@@ -21,7 +21,10 @@ get_header(); ?>
 
       </header><!-- .page-header -->
 
-      <div id='member-discount'>
+      <p>Take advantage of our partner discounts.
+      As a Member of the Empire Tri Club, you are entitled to discounts from many of our partners and sponsors. Please note that many of our partner shops require you to show your Empire Tri Club Member ID in order to receive discounts. These discounts are for Empire Tri Club Members only, so please use them accordingly so we don’t lose these great privileges!</p>
+
+      <div id='member-discounts'>
 
       <?php get_template_part( 'loop' ); ?>
 
